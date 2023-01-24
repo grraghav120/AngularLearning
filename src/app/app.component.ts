2178954e8493ded5c4bdda922c6c2a2b0270dd49
RAGHAV GARG <raghavgarg.hcst.cs20@sgei.org>
2023-01-24 16:53:31 +0530

    pushing code

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,14 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = 'firstApp';
+  name = 'Raghav Garg';
+  branch = 'Computer Science Engineering';
+  collegeName = 'Hindustan College of Science and Technology';
+  course='Bachelor of Technology';
+  constructor(){
+    this.name='Raghav';
+  }
 }
